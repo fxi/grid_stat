3a704186636e1cfd2a0f9ecb48289c9163243058
fxi <f@fxi.io>
2024-10-17 17:28:27 +0200

    add vite build base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/grid_stat/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
